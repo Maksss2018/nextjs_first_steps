3c10a062c96c72e317a2642e154bd0795063d890
Maksym Malyi <malyimaksym37@gmail.com>
2025-04-28 15:01:09 +0300

    removed main layout from client layout

--- app/client/[clientId]/layout.js
+++ app/client/[clientId]/layout.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import styles from "./page.module.css";
+import "../../globals.css";
+import styles from "../../page.module.css";
 import Link from "next/link";
 
 const geistSans = Geist({
@@ -20,63 +20,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div className={styles.page}>
-          <nav>
-            <Link href={"/"}>Main page</Link>
-            <br />
-            <Link href={"/client"}>Client page</Link>
-            <br />
-          </nav>
-          <main className={styles.main}>{children}</main>
-          <footer className={styles.footer}>
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/file.svg"
-                alt="File icon"
-                width={16}
-                height={16}
-              />
-              Learn
-            </a>
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/window.svg"
-                alt="Window icon"
-                width={16}
-                height={16}
-              />
-              Examples
-            </a>
-            <a
-              href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/globe.svg"
-                alt="Globe icon"
-                width={16}
-                height={16}
-              />
-              Go to nextjs.org →
-            </a>
-          </footer>
-        </div>
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
