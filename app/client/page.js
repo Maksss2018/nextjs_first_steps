77966b128186c7ad599d1a4cf2201766ca42a2f2
Maksym Malyi <malyimaksym37@gmail.com>
2025-04-28 15:31:15 +0300

    fetching list of users

--- app/client/page.js
+++ app/client/page.js
@@ -1,3 +1,22 @@
-export default function Client() {
-  return <h1>Client page</h1>;
+import Link from "next/link";
+async function fetchUsers() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+    response.json()
+  );
+}
+export default async function Client() {
+  const users = await fetchUsers();
+  return (
+    <>
+      <h1>Client page</h1>
+      {users.map((user) => (
+        <section key={user.id}>
+          <h2>
+            {user.name} - <b>{user.username}</b>{" "}
+          </h2>
+          <Link href={`/client/${user.id}`}> more </Link>
+        </section>
+      ))}
+    </>
+  );
 }
