98e7f6f55ca05b7f47781fb475937fa723080b94
Maksym Malyi <malyimaksym37@gmail.com>
2025-04-28 15:02:00 +0300

    clean layout file for client page

--- app/client/[clientId]/layout.js
+++ app/client/[clientId]/layout.js
@@ -1,19 +1,3 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../../globals.css";
-import styles from "../../page.module.css";
-import Link from "next/link";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Get Client by id",
   description: "getting client by id in the url",
